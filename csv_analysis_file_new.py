0dc75187844c0be73abf43ec1cc9220a87774ac1
Gili Wolf <giliwof1999@gmail.com>
2024-02-08 10:51:55 +0200

    small changes in csv_analysis_file_new.py

--- csv_analysis_file_new.py
+++ csv_analysis_file_new.py
@@ -23,7 +23,7 @@ def extract_start_and_end_position(pos_of_editing_site, range_of_interest):
     # the interesting part - probably 40 bases for each site
     start_position = pos_of_editing_site - range_of_interest
     end_position = pos_of_editing_site + range_of_interest
-    return start_position. end_position
+    return start_position, end_position
 # the func's output is two lists in which the first one is the sites evolving bases and the second one is the site's structures
 # it uses "iterate string"
 def create_list_of_features(cur_sequence, features, start_position, end_position):   
@@ -41,13 +41,15 @@ def iterate_string(string_to_iterate, start, end, list_to_add):
 
 def extract_feature_of_editing_site():
     pass 
+
+
 def check_strand_identity(coords_of_segment):
     return start_of_first_strand, end_of_first_strand, start_of_sec_strand, end_of_sec_strand, editing_on_first_strand, editing_on_sec_strand
 
 # create the data frame general structure 
 def create_df_structure(csv_file_path, overwrite=False):
     # Specify the column names
-    column_names = ["editing location feature", "number of I", "number of S", "number of H", "number of E", "number of B", "number of M"]
+    column_names = ["editing location site", "number of I", "number of S", "number of H", "number of E", "number of B", "number of M", "editing site base", "editing base on the other strand"]
     # Check if the file already exists
     if os.path.exists(csv_file_path) and not overwrite:
         raise FileExistsError(f"File '{csv_file_path}' already exists. Set 'overwrite=True' to overwrite.")
@@ -61,7 +63,7 @@ def fill_df_with_data(csv_file_path, list_of_features):
     # call function that extracts nuc's structural feature
     # @@ - we need to add editing location feature!!!
     new_row = {
-    "editing location feature": "example",
+    "editing location site": "example",
     "number of I": i_count,
     "number of S": s_count,
     "number of H": h_count,
@@ -95,15 +97,22 @@ def categorize_features(list_of_features):
             b_count += 1
     return m_count, s_count, h_count, i_count, e_count, b_count
 
+def create_csv_file_path():
+
 def mini_main():
     # the relevant st_file
-    st_file_path = "some_path"
+    st_file_path = "/private10/Projects/Reut_Shelly/our_tool/data/sites_of_interest_analysis/chr1_10003145/default_tool/10003145_default_tool_mxfolded.st"
     pos_of_editing_site = 100
     range_of_interest = 40
     # split st_file by lines
-    name, seq_length, cur_sequence, cur_structure, features = process_st_file_by_lines(st_file)
+    name, seq_length, cur_sequence, cur_structure, features = process_st_file_by_lines(st_file_path)
     # extract start position and end position
+    # extract list of features, list of seq
     start_position, end_position = extract_start_and_end_position(pos_of_editing_site, range_of_interest)
     list_of_seq, list_of_features = create_list_of_features(cur_sequence, features, start_position, end_position)
-    create_df_structure(st_file_path, overwrite=False)
+    # create csv file path
+    csv_file_path = "some_csv_file_path"
+    # create df structure
+    create_df_structure(csv_file_path, overwrite=False)
+    # fill df with data, based on list of features
     fill_df_with_data(csv_file_path, list_of_features)
