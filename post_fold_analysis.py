6bdfb93bc5d2fd176f9380995ec19aaa494eab28
Gili Wolf <giliwof1999@gmail.com>
2024-08-01 16:37:42 +0300

    add comments to original post_fold_analysis

--- post_fold_analysis.py
+++ post_fold_analysis.py
@@ -26,24 +26,26 @@ def parse_st_file(st_file, location_of_site):
     if not os.path.exists(st_file):
         print(f"Error: The file {st_file} does not exist.")
         return coords_of_segment, seqs_of_segment, segment, length
-
+    # open the file
     with open(st_file, "r") as bpf:
         data = bpf.readlines()
     
+    # regex: 123...456
     regex = re.compile(r"(\s\d+\.\.\d+\s)")
     for line in data:
+        # if we are in the bottom part of the file which looks like this: "segment1 3bp 12..14 AGG 878..880 CCU"
         if "segment" in line:
-            l = regex.split(line)
-            segment_and_length = l[0]
+            l = regex.split(line)    # split the line this way: ['segment1 3bp', ' 12..14 ', 'AGG', ' 878..880 ', 'CCU']
+            segment_and_length = l[0] # 'segment1 3bp'
             split_segment_and_length = segment_and_length.split()
-            segment = split_segment_and_length[0]
-            length = split_segment_and_length[1]
-            range1 = l[1].strip()
+            segment = split_segment_and_length[0] # segment1
+            length = split_segment_and_length[1] # 3bp
+            range1 = l[1].strip() # remove spaces from '  12..14  '
             range1 = {
-                "start": int(range1.split(".")[0]), 
-                "end": int(range1.split(".")[-1]), 
+                "start": int(range1.split(".")[0]), # 12
+                "end": int(range1.split(".")[-1]), # 14
             }
-            range2 = l[3].strip()
+            range2 = l[3].strip() # same with the second range
             range2 = {
                 "start": int(range2.split(".")[0]),
                 "end": int(range2.split(".")[-1]),
@@ -52,13 +54,14 @@ def parse_st_file(st_file, location_of_site):
             if (range1["start"] <= location_of_site + 1 <= range1["end"]) or (
                 range2["start"] <= location_of_site + 1 <= range2["end"]
             ):
+                # the requested segment
                 coords_of_segment = (
                     range1["start"],
                     range1["end"],
                     range2["start"],
                     range2["end"],
                 )
-                seqs_of_segment = (l[2], l[-1].strip("\n"))
+                seqs_of_segment = (l[2], l[-1].strip("\n")) # seqs_of_segment is ('AGG', 'CCU')
                 break  # Ensures that you stop searching once a match is found
 
     return coords_of_segment, seqs_of_segment, segment, length
@@ -68,4 +71,7 @@ def extract_segment(start, end, st_path, location_of_site):
     print (f"the new start is : {new_start} ,the new end is: {new_end} ,the new location of site is: {new_location_of_site}")
     # coords of the location of site's segment
     coords_of_segment, seqs_of_segment, segment, length = parse_st_file(st_path, new_location_of_site)
+    # convert_to_genomic_coords(coords_of_segment, seqs_of_segment, segment, length)
     print (coords_of_segment, seqs_of_segment, segment, length)
+
+# def convert_to_genomic_coords(coords_of_segment, seqs_of_segment, segment, length):
