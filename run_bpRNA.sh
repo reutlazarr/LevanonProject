1e5387692d6020ced150a2f30a40a3c93ae3e362
Gili Wolf <giliwof1999@gmail.com>
2024-08-23 10:30:25 +0300

    add bpRNA

--- run_bpRNA.sh
+++ run_bpRNA.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
  
-/home/alu/twerski/anaconda3/envs/foldRNA/bin/perl "/private6/Projects/Yeast_Itamar_10_2022/Fold_energy/bpRNA/bpRNA.pl" $1
+/home/alu/twerski/anaconda3/envs/foldRNA/bin/perl "/home/alu/aluguest/Reut_Shelly/vscode/code_reut/LevanonProject/LevanonProject/copiedbpRNA.pl" $1
